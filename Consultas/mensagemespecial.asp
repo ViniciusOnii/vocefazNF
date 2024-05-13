4ef1e06d1ed77dd2f2ce0aaeba60f5bbb0413f1a
ViniciusOnii <viniciusonii@outlook.com>
2024-05-13 09:59:04 -0300

    tela estado uf criadatela mensagem nf criada

---
+++ Consultas/mensagemespecial.asp
@@ -0,0 +1,144 @@
+﻿<!-- Inclui o arquivo de configuração ADO -->
+<!--#include file="adovbs.inc"-->
+<!--#include file="config.asp"-->
+
+<%
+     Dim tmensagem
+     Dim vflag_dados 
+     
+     ' Variável para controlar se houve registros selecionados, para voltar o ponteiro para o início dos dados
+     vflag_dados=0
+           
+     ' Busca o Cupom na Base de Dados
+     Set tmensagem = Server.CreateObject("ADODB.Recordset")
+     tmensagem.CursorType = adOpenDynamic
+     tmensagem.LockType = adLockOptimistic
+
+     ' Abre o recordset com os dados dos subgrupos ordenados pelo código do subgrupo
+     tmensagem.open "SELECT * FROM mensagemespecial ORDER BY codigo_mensagem_especial" ,erpDB
+
+     ' Se houver registros, define a flag de dados como 1
+     if Not tmensagem.eof then vflag_dados=1
+%>
+
+<!-- Monta a seção para desktop -->
+<div class="container-fluid">
+    <div class="estilo_desktop">
+
+        <!-- Cabeçalho com a descrição das colunas da consulta -->
+        <div class="row" style="width:100%;height:30px;background-color:black;color:white;margin:0px;padding:0px;font-family:Arial">
+            <div class="col-2" style="font-size:18px">Codigo</div>
+            <div class="col-10" style="font-size:18px">Mensagem</div>
+        </div>
+
+        <div class="row" style="width:100%;height:40px"></div>
+
+        <% 
+            ' Inicializa a variável de controle de linha
+            vlinha=1
+
+            ' Loop para iterar sobre os registros do recordset
+            Do while Not tmensagem.eof
+        %>
+     
+        <div id="linha_<%=vlinha %>" class="row" style="width:100%;height:30px;background-color:transparent;color:black;margin:0px;padding:0px;font-family:Arial;cursor:pointer" onmouseover="altera_fundo_linha(this,1)" onmouseout="altera_fundo_linha(this,0)">
+            <div class="col-2" style="font-size:18px;cursor:pointer" ondblclick="resposta_consulta('<%=tmensagem("codigo_mensagem_especial") %>')"><%=tmensagem("codigo_mensagem_especial") %></div>
+            <div class="col-10" style="font-size:18px;cursor:pointer" ondblclick="resposta_consulta('<%=tmensagem("codigo_mensagem_especial") %>')"><%=tmensagem("descricao_mensagem_especial") %></div>
+        </div>
+
+        <div class="row" style="width:100%;height:1px;background-color:black;padding:0px;margin:0px"></div>
+        <div class="row" style="width:100%;height:10px;background-color:transparent"></div>
+
+        <% 
+            ' Incrementa o número da linha
+            vlinha=vlinha+1
+
+            ' Move para o próximo registro
+            tmensagem.MoveNext
+        Loop
+        %>
+
+    </div>
+
+    <!-- Verifica se há dados e retorna o ponteiro para o início do recordset -->
+    <%if vflag_dados=1 then tmensagem.MoveFirst %>
+
+    <!-- Monta a seção para tablet -->
+    <div class="estilo_tablet">
+
+        <!-- Cabeçalho com a descrição das colunas da consulta -->
+        <div class="row" style="width:100%;height:30px;background-color:black;color:white;margin:0px;padding:0px;font-family:Arial">
+            <div class="col-2" style="font-size:18px">Codigo</div>
+            <div class="col-10" style="font-size:18px">Mensagem</div>
+        </div>
+
+        <div class="row" style="width:100%;height:40px"></div>
+
+        <% 
+            ' Reset da variável de controle de linha
+            vlinha=1
+
+            ' Loop para iterar sobre os registros do recordset
+            Do while Not tmensagem.eof
+        %>
+     
+        <div id="linha_<%=vlinha %>_tablet" class="row" style="width:100%;height:30px;background-color:transparent;color:black;margin:0px;padding:0px;font-family:Arial;cursor:pointer" onmouseover="altera_fundo_linha(this,1)" onmouseout="altera_fundo_linha(this,0)">
+            <div class="col-2" style="font-size:18px;cursor:pointer" ondblclick="resposta_consulta('<%=tmensagem("codigo_mensagem_especial") %>')"><%=tmensagem("codigo_mensagem_especial") %></div>
+            <div class="col-10" style="font-size:18px;cursor:pointer" ondblclick="resposta_consulta('<%=tmensagem("codigo_mensagem_especial") %>')"><%=tmensagem("descricao_mensagem_especial") %></div>
+        </div>
+
+        <div class="row" style="width:100%;height:1px;background-color:black;padding:0px;margin:0px"></div>
+        <div class="row" style="width:100%;height:10px;background-color:transparent"></div>
+
+        <% 
+            ' Incrementa o número da linha
+            vlinha=vlinha+1
+
+            ' Move para o próximo registro
+            tsubgrupo.MoveNext
+        Loop
+        %>
+
+    </div>
+
+    <!-- Verifica se há dados e retorna o ponteiro para o início do recordset -->
+    <%if vflag_dados=1 then tmensagem.MoveFirst %>
+
+    <!-- Monta a seção para mobile -->
+    <div class="estilo_mobile">
+
+        <!-- Cabeçalho com a descrição das colunas da consulta -->
+        <div class="row" style="width:100%;height:30px;background-color:black;color:white;margin:0px;padding:0px;font-family:Arial">
+            <div class="col-2" style="font-size:14px">Codigo</div>
+            <div class="col-10" style="font-size:14px">Mensagem</div>
+        </div>
+
+        <div class="row" style="width:100%;height:40px"></div>
+
+        <% 
+            ' Reset da variável de controle de linha
+            vlinha=1
+
+            ' Loop para iterar sobre os registros do recordset
+            Do while Not tmensagem.eof
+        %>
+     
+        <div id="linha_<%=vlinha %>_mobile" class="row" style="width:100%;height:30px;background-color:transparent;color:black;margin:0px;padding:0px;font-family:Arial;cursor:pointer" onmouseover="altera_fundo_linha(this,1)" onmouseout="altera_fundo_linha(this,0)">
+            <div class="col-2" style="font-size:14px;cursor:pointer" ondblclick="resposta_consulta('<%=tmensagem("codigo_mensagem_especial") %>')"><%=tmensagem("codigo_mensagem_especial") %></div>
+            <div class="col-10" style="font-size:14px;cursor:pointer" ondblclick="resposta_consulta('<%=tmensagem("codigo_mensagem_especial") %>')"><%=tmensagem("descricao_mensagem_especial") %></div>
+        </div>
+
+        <div class="row" style="width:100%;height:1px;background-color:black;padding:0px;margin:0px"></div>
+        <div class="row" style="width:100%;height:10px;background-color:transparent"></div>
+
+        <% 
+            ' Incrementa o número da linha
+            vlinha=vlinha+1
+
+            ' Move para o próximo registro
+            tmensagem.MoveNext
+        Loop
+        %>
+
+    </div>
+</div>
---
+++ telas/estadosuf.aspx.vb
@@ -0,0 +1,265 @@
+﻿Public Class estadosuf1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub cmd_gravar_Click(sender As Object, e As EventArgs) Handles cmd_gravar.Click
+        Dim verp As New Data.SqlClient.SqlConnection
+        Dim vcmdcomando As New Data.SqlClient.SqlCommand
+        Dim vcmdcomando_gravar As New Data.SqlClient.SqlCommand
+        Dim testado As Data.SqlClient.SqlDataReader
+
+        Dim vselecao As String
+        Dim vsigla_estado As String
+        Dim vdescricao_estado As String
+        Dim vicms_interno As String
+        Dim vicms_interestadual As String
+        Dim vflag_obrigatorio As Integer
+
+        Try
+            vselecao = 0
+            vflag_obrigatorio = 0
+
+            vsigla_estado = Trim(cs_sigla_estado.Text)
+            vdescricao_estado = Trim(cs_descricao_estado.Text)
+            vicms_interno = Trim(cs_icms_interno.Text)
+            vicms_interestadual = Trim(cs_icms_interestadual.Text)
+            '///////////////////////////// Consistências //////////////////////
+            If Trim(vsigla_estado) = "" Then
+                cs_sigla_estado.BackColor = Drawing.Color.Red
+                vflag_obrigatorio = 1
+            Else
+                cs_sigla_estado.BackColor = Drawing.Color.Transparent
+            End If
+
+            If Trim(vdescricao_estado) = "" Then
+                cs_descricao_estado.BackColor = Drawing.Color.Red
+                vflag_obrigatorio = 1
+            Else
+                cs_descricao_estado.BackColor = Drawing.Color.Transparent
+            End If
+
+            If Trim(vicms_interno) = "" Then
+                cs_icms_interno.BackColor = Drawing.Color.Red
+                vflag_obrigatorio = 1
+            Else
+                cs_icms_interno.BackColor = Drawing.Color.Transparent
+            End If
+
+            If Trim(vicms_interestadual) = "" Then
+                cs_icms_interestadual.BackColor = Drawing.Color.Red
+                vflag_obrigatorio = 1
+            Else
+                cs_icms_interestadual.BackColor = Drawing.Color.Transparent
+            End If
+
+
+            If (vflag_obrigatorio = 1) Then
+                pnl_principal.Visible = False
+                pnl_obrigatorio.Visible = True
+                Exit Sub
+
+            End If
+
+
+            '////// abre o banco de dados ////////////////////////
+            ' Abrir conexão com o banco de dados
+            verp.ConnectionString = My.Settings.vconexaobase
+            verp.Open()
+
+            vselecao = "SELECT * FROM estadouf WHERE sigla_estado_uf='" & Trim(vsigla_estado) & "'"
+
+            vcmdcomando.CommandText = vselecao
+            vcmdcomando.CommandType = CommandType.Text
+            vcmdcomando.CommandTimeout = 0
+            vcmdcomando.Connection = verp
+
+            testado = vcmdcomando.ExecuteReader
+
+            If testado.Read() Then
+                vselecao = "UPDATE estadouf SET "
+                vselecao = vselecao & "descricao_estado='" & Trim(vdescricao_estado) & "', "
+                vselecao = vselecao & "icms_interno='" & Trim(vicms_interno) & "', "
+                vselecao = vselecao & "icms_interestadual='" & Trim(vicms_interestadual) & "' " ' Adicionando as novas colunas
+                vselecao = vselecao & "WHERE sigla_estado_uf='" & Trim(vsigla_estado) & "'"
+                testado.Close()
+
+                vcmdcomando_gravar.CommandText = vselecao
+                vcmdcomando_gravar.CommandType = CommandType.Text
+                vcmdcomando_gravar.CommandTimeout = 0
+                vcmdcomando_gravar.Connection = verp
+                vcmdcomando_gravar.ExecuteNonQuery()
+
+            Else
+
+                vselecao = "INSERT INTO estadouf("
+                vselecao = vselecao & "sigla_estado_uf,"
+                vselecao = vselecao & "descricao_estado,"
+                vselecao = vselecao & "icms_interno," ' Adicionando a nova coluna
+                vselecao = vselecao & "icms_interestadual)" ' Adicionando a nova coluna
+                vselecao = vselecao & "VALUES("
+                vselecao = vselecao & "'" & vsigla_estado & "',"
+                vselecao = vselecao & "'" & vdescricao_estado & "',"
+                vselecao = vselecao & "'" & vicms_interno & "'," ' Adicionando a nova coluna
+                vselecao = vselecao & "'" & vicms_interestadual & "')" ' Adicionando a nova coluna
+                testado.Close()
+
+                vcmdcomando_gravar.CommandText = vselecao
+                vcmdcomando_gravar.CommandType = CommandType.Text
+                vcmdcomando_gravar.CommandTimeout = 0
+                vcmdcomando_gravar.Connection = verp
+                vcmdcomando_gravar.ExecuteNonQuery()
+
+            End If
+
+
+            ' Fechando a conexão com o banco de dados
+            verp.Close()
+
+            cmd_gravar.Visible = False
+            cmd_deletar.Visible = False
+            cs_sigla_estado.Text = ""
+            cs_descricao_estado.Text = ""
+            cs_icms_interno.Text = ""
+            cs_icms_interestadual.Text = ""
+            cs_sigla_estado.Focus()
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    Protected Sub cmd_deletar_Click(sender As Object, e As EventArgs) Handles cmd_deletar.Click
+        Dim verp As New Data.SqlClient.SqlConnection
+        Dim vcmdcomando As New Data.SqlClient.SqlCommand
+        Dim vcmdcomando_deletar As New Data.SqlClient.SqlCommand
+        Dim testado As Data.SqlClient.SqlDataReader
+
+
+        Dim vselecao As String
+        Dim vflag_obrigatorio As Integer
+        Dim vsigla_estado As String
+
+
+        Try
+            vflag_obrigatorio = 0
+            vsigla_estado = Trim(cs_sigla_estado.Text)
+
+
+            '///////////////////////////// Consistências /////////////////////
+            If Trim(vsigla_estado) = "" Then
+                cs_sigla_estado.BackColor = Drawing.Color.Red
+                vflag_obrigatorio = 1
+            Else
+                cs_sigla_estado.BackColor = Drawing.Color.Transparent
+            End If
+
+
+            '////// abre o banco de dados ///////////////////////////////////////
+            verp.ConnectionString = My.Settings.vconexaobase
+            verp.Open()
+
+            vselecao = "SELECT * FROM estadouf WHERE descricao_estado='" & Trim(vsigla_estado) & "'"
+
+            vcmdcomando.CommandText = vselecao
+            vcmdcomando.CommandType = CommandType.Text
+            vcmdcomando.CommandTimeout = 0
+            vcmdcomando.Connection = verp
+
+            testado = vcmdcomando.ExecuteReader
+
+            If testado.Read() Then
+
+                vselecao = "DELETE FROM estadouf"
+                vselecao = vselecao & "WHERE descricao_estado='" & Trim(vsigla_estado) & "'"
+
+                testado.Close()
+
+
+                vcmdcomando_deletar.CommandText = vselecao
+                vcmdcomando_deletar.CommandType = CommandType.Text
+                vcmdcomando_deletar.CommandTimeout = 0
+                vcmdcomando_deletar.ExecuteNonQuery()
+
+                cs_sigla_estado.Text = ""
+                cs_descricao_estado.Text = ""
+
+
+                cmd_gravar.Visible = False
+                cmd_deletar.Visible = False
+
+            Else
+                '///erro 
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Protected Sub cmd_voltar_obrigatorio_Click(sender As Object, e As EventArgs) Handles cmd_voltar_obrigatorio.Click
+        pnl_obrigatorio.Visible = False
+        pnl_principal.Visible = True
+    End Sub
+
+    Protected Sub cs_sigla_estado_TextChanged(sender As Object, e As EventArgs) Handles cs_sigla_estado.TextChanged
+        Dim verp As New Data.SqlClient.SqlConnection
+        Dim vcmdcomando As New Data.SqlClient.SqlCommand
+        Dim testado As Data.SqlClient.SqlDataReader
+
+        Dim vselecao As String
+        Dim vsigla_estado As String
+
+        Try
+            vsigla_estado = UCase(Trim(cs_sigla_estado.Text))
+            cs_sigla_estado.Text = vsigla_estado
+
+
+            '////// abre o banco de dados ////////////////////
+            verp.ConnectionString = My.Settings.vconexaobase
+            verp.Open()
+
+            vselecao = "SELECT * FROM estadouf WHERE sigla_estado_uf='" & Trim(vsigla_estado) & "'"
+
+            vcmdcomando.CommandText = vselecao
+            vcmdcomando.CommandType = CommandType.Text
+            vcmdcomando.CommandTimeout = 0
+            vcmdcomando.Connection = verp
+
+            testado = vcmdcomando.ExecuteReader
+
+            If testado.Read() Then
+
+                cs_descricao_estado.Text = testado("descricao_estado")
+                cmd_deletar.Visible = True
+                cmd_gravar.Visible = True
+
+            Else
+                cs_descricao_estado.Text = ""
+                cmd_deletar.Visible = False
+                cmd_gravar.Visible = True
+
+
+            End If
+
+            cs_descricao_estado.Focus()
+            testado.Close()
+            verp.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Protected Sub cmd_imprimir_Click(sender As Object, e As EventArgs) Handles cmd_imprimir.Click
+        cmd_gravar.Visible = False
+        cmd_deletar.Visible = False
+
+        cs_sigla_estado.Text = ""
+        cs_descricao_estado.Text = ""
+        cs_icms_interno.Text = ""
+        cs_icms_interestadual.Text = ""
+
+        cs_sigla_estado.Focus()
+    End Sub
+End Class
